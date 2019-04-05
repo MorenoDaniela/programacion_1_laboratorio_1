3fa0ffba5c89a81b7073cdc9514218859185649c
alumno <alumno@LAB00PC00>
2019-04-04 21:19:17 -0300

    Clase 05

---
+++ arrays/funciones.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+#include "funciones.h"
+int getArray(int* arrayPResultado,int limite,const char *mensaje,const char *mensajeError,int minimo,int maximo,int reintentos)
+{
+
+    int i;
+    int num;
+    int ret;
+    for (i=0;i<limite;i++)
+    {
+        ret = utn_getNumero(&num,"Ingrese Numero: ","Error al Ingresar Numero",1,10,3);
+        if (ret)
+        {
+            break;
+        }
+        else
+        {
+        *(arrayPResultado+1)= num;
+        }
+
+    }
+    return ret;
+}
+
+/*int utn_getNumero(int* pResultado,const char *mensaje,const char *mensajeError,int minimo,int maximo,int reintentos)
+{
+    int ret;
+    int num;
+
+    while (reintentos>0)
+    {
+        printf("%s",mensaje);
+        if (getInt(&num)==0)
+        {
+            if (num<=maximo && num>=minimo)
+            {
+                break;
+            }
+        }
+                __fpurge(stdin);
+                reintentos--;
+                printf("%s\n",mensajeError);
+    }
+    if(reintentos==0)
+    {
+        ret=-1;
+    }else
+        {
+            ret=0;
+            *pResultado = num;
+        }
+    return ret;
+}*/
+
+/*int getInt(int *pResultado)
+{
+    char buffer[64];
+    scanf("%s",buffer);
+
+    *pResultado = atoi(buffer);
+    return 0;
+}*/
+
+int utn_getFloat(float *pResultado,const char *mensaje,const char *mensajeError,int minimo,int maximo,int reintentos)
+{
+    int ret;
+    float num;
+
+    while (reintentos>0)
+    {
+        printf("%s",mensaje);
+        if (getFloat(&num)==0)
+        {
+            if (num<=maximo && num>=minimo)
+            {
+                break;
+            }
+        }
+                __fpurge(stdin);
+                reintentos--;
+                printf("%s\n",mensajeError);
+    }
+    if(reintentos==0)
+    {
+        ret=-1;
+    }else
+        {
+            ret=0;
+            *pResultado = num;
+        }
+    return ret;
+}
+
+int getFloat(float *pResultado)
+{
+    char buffer[64];
+    scanf("%s",buffer);
+
+    *pResultado = atof(buffer);
+    return 0;
+}
+
+int utn_getChar(char* pResultado,const char *mensaje,const char *mensajeError,const char *minimo,const char *maximo,int reintentos)
+{
+    int ret = -1;
+    char buffer[64];
+    int i;
+
+    do{
+        printf("%s",mensaje);
+        scanf("%s", buffer);
+        i=0;
+        while (buffer[i]!='\0' && i<2)
+        {
+
+            if (buffer[0]>=minimo[0] && buffer[0]<=maximo[0])
+            {
+                ret=0;
+            }
+            i++;
+        }
+
+        if (ret==0 && i==1)
+        {
+            pResultado = buffer;
+            break;
+        }
+        reintentos--;
+        printf("%s\n",mensajeError);
+        ret = -1;
+        __fpurge(stdin);
+
+    }while (reintentos>0);
+    return ret;
+}
+
+int getInt (char* msg, char* msgErr, int maximo, int minimo, int reintentos, int* resultado)
+
+int getArrayInt (char* msg, char* msgError, int maximo, int minimo, int reintentos, int* resultado, int limite)
+{
+        int retorno=-1;
+        int i;
+        int buffer;
+
+        if (resultado!=NULL &&msg!=NULL &&msgError!=NULL && minimo<=maximo && reintentos>=0 && limite>0)
+        {
+            retorno =0;
+            for (i=0;i<limite;i++)
+            {
+                if (utn_getNumero(msg, msgError, maximo, minimo, reintentos)==0)
+                {
+                    resultado [i]=buffer;
+                    //return 0;
+                }
+                else
+                {
+                    return retorno;
+                    //break;
+                }
+            }
+        }
+
+        return retorno;
+}
+
+void printArrayInt (int* arrayResultado, int limite)
+{
+    int i;
+    for (i=o;i<limite;i++)
+    {
+        printf ("%d\n", arrayResultado[i]);
+
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ arrays/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define CANTIDAD_ALUMNOS 55
+
+int main()
+{
+    int* pEdades;//puntero a edades
+    int bufferEdad;
+    int aEdades [CANTIDAD_ALUMNOS];//no poner un numero adentro de [] mejor definir arriba con define
+
+    aEdades [100] =22;//le digo que en la posicion 100 guarde el num 22
+    bufferEdad = aEdades [400];//en el buffer se guarda lo que hay en la posicion 400 por ejemp
+    pEdades = &aEdades[88];// es lo mismo que pEdades = aEdades;
+    //el nombre de un array indica el indice la primer posicion de un elemento
+
+    pEdades = aEdades;
+    pEdades [88] = 11;//esta opcion es lo mismo que *(pEdades+88) =11; pero es peor
+    bufferEdad = pEdades[188];//esto es lo mismo que la linea 20
+    *(pEdades+88) = 11;//en el lugar donde estaba antes +88 posiciones ahi quiero escribir
+    bufferEdad = *(pEdades+188);//esta linea es lo mismo que la linea 18
+    //tipo de array y tipo de puntero TIENEN QUE SER IGUALES EN EL TIPO (int, char, float)
+
+    int getInt (char* msg, char* msgErr, int maximo, int minimo, int reintentos, int* resultado)
+
+    int getArrayInt (char* msg, char* msgErr, int maximo, int minimo, int reintentos, int* ArrayResultado, int limite)
+    //llama a la funcion de arriba, en la funcion de arriba se declara un buffer y se le pasa a esta y recien cuando se que no es un error scaneo
+
+}
