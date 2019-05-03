3bda303d0332a1f9a936ba06c206f6d813a5ade0
MorenoDaniela <47337445+MorenoDaniela@users.noreply.github.com>
2019-05-02 22:19:39 -0300

    Add files via upload

---
+++ ABM_Pantalla_2/incluye.h
@@ -0,0 +1,7 @@
+#ifndef INCLUYE_H_INCLUDED
+#define INCLUYE_H_INCLUDED
+
+int mostrarMenorADiez (Pantalla* arrayPan,int limitePan, Publicidad* arrayPub,int limitePub);
+
+
+#endif // INCLUYE_H_INCLUDED
